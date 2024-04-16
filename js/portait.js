010a303fb52e8a42411df8ce7789ecb929264436
Cowry11 <1572134778@qq.com>
2024-04-16 20:46:22 +1000

    1

---
+++ js/portait.js
@@ -0,0 +1,37 @@
+function setup() {
+    createCanvas(windowWidth,windowHeight)
+    angleMode(DEGREES)
+    rectMode(CENTER)
+    background(178, 219, 145)
+    fill(0)
+    rect(967,640,695,910,291)
+    fill(255,242,222)
+    arc(715,600,100,90,90,270)
+    arc(1220,600,100,90,270,90)
+    fill(255,242,222)
+    ellipse(967,600,510,595)
+    fill(0)
+    rect(960,379,460,153,358)
+    fill(74,44,0)
+    ellipse(850,600,55,55)
+    ellipse(1090,600,55,55)
+    fill(0)
+    ellipse(850,600,25,25)
+    ellipse(1090,600,25,25)
+    fill(255)
+    ellipse(842,600,11,18)
+    ellipse(1082,600,11,18)
+    fill(255, 66, 28)
+    arc(968,770,80,70,0,180)
+    noFill()
+    arc(968,705,25,25,260,100)
+    stroke(51,26,0)
+    strokeWeight(11)
+    line(800,515,895,515)
+    line(1045,515,1140,515)
+  }
+  
+  
+  function draw() {
+  
+  }
